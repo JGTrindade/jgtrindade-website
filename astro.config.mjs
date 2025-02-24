62ccb250a6c27f34ea42628a889aa3b61c498760
JGTrindade <jgabriel.master@gmail.com>
2025-02-24 09:06:57 -0300

    Started internationalization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
+export default defineConfig({
+  i18n: {
+    locales: ["ar", "en", "es", "fr", "pt-br", "ru", "zh-cn"],
+    defaultLocale: "en",
+    routing: {
+        prefixDefaultLocale: false
+    }
+  }
+})
